f198dcf708daf33c15e8b56f52045e1e1d534135
Rahul Roshan <rahulr@avinetworks.com>
2019-03-27 17:34:10 +0530

    Fibonacci BottomUP approach added

--- src/DP/Fibonacci.java
+++ src/DP/Fibonacci.java
@@ -8,9 +8,10 @@ public class Fibonacci {
     public static void main(String args[]){
         Fibonacci hs = new Fibonacci();
         Map<Integer, Integer> mm = new HashMap<Integer, Integer>();
-        System.out.println(hs.getFib(10, mm));
+        System.out.println(hs.getFibTopDown(10, mm));
+        System.out.println(hs.getFibBottomUp(10));
     }
-    int getFib(int n, Map<Integer,Integer> mm){
+    int getFibTopDown(int n, Map<Integer,Integer> mm){
         if(n==1 || n==2){
             return 1;
         }else{
@@ -20,5 +21,17 @@ public class Fibonacci {
             mm.put(n,(getFib(n-1,mm) + getFib(n-2,mm)));
             return mm.get(n);
         }
+    }
+    int getFibBottomUp(int n){
+        int a=0,b=1;
+        int c=0;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        for(int i=2;i<=n;i++){
+            c = a+b;
+            a=b;
+            b=c;
+        }
+        return c;
     }
 }
